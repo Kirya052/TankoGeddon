41ddc93b38d58c1e1e75dcdf4cf9bc1cf748a8c7
Kirya052 <kiryakonev01@mail.ru>
2022-06-30 14:03:19 +0500

    Add Tank meshes. Create rotate and Fire logic

--- TankoGeddon/Source/TankoGeddon/TankPawn.cpp
+++ TankoGeddon/Source/TankoGeddon/TankPawn.cpp
@@ -5,6 +5,9 @@
 #include "Components\StaticMeshComponent.h"
 #include "GameFramework\SpringArmComponent.h"
 #include "Camera\CameraComponent.h"
+#include "TankController.h"
+#include "Kismet\KismetMathLibrary.h"
+#include "Cannon.h"
 
 
 ATankPawn::ATankPawn()
@@ -19,6 +22,11 @@ ATankPawn::ATankPawn()
 
 	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
 	SpringArm->SetupAttachment(BodyMesh);
+	SpringArm->bDoCollisionTest = false;
+	SpringArm->bInheritPitch = false;
+	SpringArm->bInheritYaw = false;
+	SpringArm->bInheritRoll = false;
+
 
 	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
 	Camera->SetupAttachment(SpringArm);
@@ -26,16 +34,91 @@ ATankPawn::ATankPawn()
 
 void ATankPawn::MoveForward(float Value)
 {
-	TargetAxisValue = Value;
+	TargetForwardAxisValue = Value;
+}
+
+void ATankPawn::MoveRight(float Value)
+{
+	TargetRightAxisValue = Value;
+}
+
+void ATankPawn::RotateRight(float Value)
+{
+	RotateRightAxisValue = Value;
+}
+
+void ATankPawn::Fire()
+{
+	if (Cannon)
+	{
+		Cannon->Fire();
+	}
 }
 
 void ATankPawn::Tick(float DeltaSeconds)
 {
 	Super::Tick(DeltaSeconds);
 
+	//tank movement
 	FVector CurrentLocation = GetActorLocation();
 	FVector forwardVector = GetActorForwardVector();
-	FVector movePosition = CurrentLocation + forwardVector * MoveSpeed * TargetAxisValue;
+	FVector rightVector = GetActorRightVector();
+	FVector movePosition = CurrentLocation + (forwardVector * MoveSpeed * TargetForwardAxisValue) + (rightVector * MoveSpeed * TargetRightAxisValue);
 	SetActorLocation(movePosition, true);
+
+	//Tank rotation
+	CurrentRotateAxisValue = FMath::Lerp(CurrentRotateAxisValue, RotateRightAxisValue, InterpolationKey);
+	float YawRotation = RotationSpeed * CurrentRotateAxisValue * DeltaSeconds;
+
+	UE_LOG(LogTemp, Warning, TEXT("CurrentRotateAxis Value: %f, RotateRightAxisValue: %f"), CurrentRotateAxisValue, RotateRightAxisValue);
+
+	FRotator CurrentRotation = GetActorRotation();
+
+	YawRotation = CurrentRotation.Yaw + YawRotation;
+	//YawRotation += CurrentRotation.Yaw;
+
+	FRotator newRotation = FRotator(0.0f, YawRotation, 0.0f);
+	SetActorRotation(newRotation);
+
+	//Turret Rotation
+	FVector MousePos = TankController->GetMousePosition();
+	
+	FRotator targetRotation = UKismetMathLibrary::FindLookAtRotation(GetActorLocation(), MousePos);
+	FRotator TurretRotation = TurretMesh->GetComponentRotation();
+
+	targetRotation.Pitch = TurretRotation.Pitch;
+	targetRotation.Roll = TurretRotation.Roll;
+
+	TurretMesh->SetWorldRotation(FMath::Lerp(TurretRotation, targetRotation, RotateInterpolationKey));
+}
+
+void ATankPawn::BeginPlay()
+{
+	Super::BeginPlay();
+
+	SetActorLocation(FVector(GetActorLocation().X, GetActorLocation().Y, 0.0f));
+
+	TankController = Cast<ATankController>(GetController());
+
+	SetupCannon();
+}
+
+void ATankPawn::SetupCannon()
+{
+	if (!CannonClass)
+	{
+		return;
+	}
+	if (Cannon)
+	{
+		Cannon->Destroy();
+	}
+	FActorSpawnParameters params;
+	params.Instigator = this;
+	params.Owner = this;
+
+	Cannon = GetWorld()->SpawnActor<ACannon>(CannonClass, params);
+
+	Cannon->AttachToComponent(TurretMesh, FAttachmentTransformRules::SnapToTargetIncludingScale);
 }
 
--- TankoGeddon/Source/TankoGeddon/TankPawn.h
+++ TankoGeddon/Source/TankoGeddon/TankPawn.h
@@ -7,6 +7,7 @@
 #include "TankPawn.generated.h"
 
 class UStaticMeshComponent;
+class ACannon;
 UCLASS()
 class TANKOGEDDON_API ATankPawn : public APawn
 {
@@ -16,8 +17,14 @@ public:
 	ATankPawn();
 
 	void MoveForward(float Value);
+	void MoveRight(float Value);
+	void RotateRight(float Value);
+
+	void Fire();
 
 	virtual void Tick( float DeltaSeconds ) override;
+
+	virtual void BeginPlay() override;
 
 protected:
 	UPROPERTY(VisibleDefaultsOnly, BlueprintReadWrite, Category = "Components")
@@ -32,9 +39,33 @@ protected:
 	UPROPERTY(VisibleDefaultsOnly, BlueprintReadWrite, Category = " Components")
 	class UCameraComponent* Camera;
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Turret | Component")
+	TSubclassOf<ACannon> CannonClass;
+
+	UPROPERTY()
+	ACannon* Cannon;
+
 	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Movement")
 	float MoveSpeed = 100.0f;
 
-	UPROPERTY(VisibleDefaultsOnly, BlueprintReadWrite, Category = "Movement")
-	float TargetAxisValue = 0.0f;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Movement")
+	float RotationSpeed = 10.0f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Movement")
+	float InterpolationKey = 0.1f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Movement")
+	float RotateInterpolationKey = 0.1f;
+
+	UPROPERTY()
+	class ATankController* TankController;
+
+	void SetupCannon();
+
+private:
+	float TargetForwardAxisValue = 0.0f;
+	float TargetRightAxisValue = 0.0f;
+
+	float RotateRightAxisValue = 0.0f;
+	float CurrentRotateAxisValue = 0.0f;
 };
